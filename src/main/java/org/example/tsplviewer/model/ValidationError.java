50e51cdc2f3cab7e8f7e40b5db217932bee96ebe
Flor Stellamans <flor@altec.be>
2025-12-30 09:30:21 +0100

    create validation error class

---
+++ src/main/java/org/example/tsplviewer/model/ValidationError.java
@@ -0,0 +1,25 @@
+package org.example.tsplviewer.model;
+
+public class ValidationError {
+
+    private final int line;
+    private final String message;
+
+    public ValidationError(int line, String message) {
+        this.line = line;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    @Override
+    public String toString() {
+        return "Line: " + line + " | " + message;
+    }
+}
